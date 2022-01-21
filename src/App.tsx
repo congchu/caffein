b50a75b331e4da93cde0cc6e060ae493e9cdb5d3
congchu <cookie00421@gmail.com>
2022-01-21 13:36:59 +0900

    [fix] 라우팅 basename 추가 및 path 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, CampDetail, CampApply, Community, NotFound } from "pages";
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/caffein">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="camp/apply" element={<CampApply />} />
-        <Route path="camp/:id" element={<CampDetail />} />
-        <Route path="community/" element={<Community />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/camp/apply" element={<CampApply />} />
+        <Route path="/camp/:id" element={<CampDetail />} />
+        <Route path="/community/" element={<Community />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
