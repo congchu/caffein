11a21ac608cd7511c0acaf20db4cff7afdf844a7
congchu <cookie00421@gmail.com>
2022-01-30 02:19:41 +0900

    [fix] 카드 리스트 비율 다른 버그 수정

--- src/pages/home/components/CampSection.tsx
+++ src/pages/home/components/CampSection.tsx
@@ -11,14 +11,10 @@ interface IProps {
   isHeadField?: boolean;
 }
 const CampSection = ({ title, camps, isHeadField = false }: IProps) => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 680px)",
-  });
-
   return (
     <Container>
       <div className="section-title">{title}</div>
-      <div className={`${isMobile ? "" : "flex"}`}>
+      <div className="camp-cards">
         {camps.map((camp, index) => (
           <CampCard key={index} camp={camp} isHeadField={isHeadField} />
         ))}
@@ -37,9 +33,14 @@ const Container = styled.section`
     padding-bottom: 8px;
   }
 
-  .flex {
-    display: flex;
-    justify-content: space-between;
-    gap: 16px;
+  .camp-cards {
+    a {
+      flex: 1;
+    }
+    @media (min-width: 680px) {
+      display: flex;
+      justify-content: space-between;
+      gap: 16px;
+    }
   }
 `;
