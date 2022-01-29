9ac9a465210c5c63d9d46c42192566104de3d566
congchu <cookie00421@gmail.com>
2022-01-30 02:10:39 +0900

    [feat] camp 데이터 server API 연동

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,20 +8,9 @@ import {
   CommunitySection,
 } from "./components";
 
-import { ICamp, ICommunity } from "types/type";
+import { CampType, ICamp, ICommunity } from "types/type";
 import { useEffect, useState } from "react";
 import { maxWidth } from "styles/mixin";
-
-const campMock: ICamp = {
-  id: 0,
-  name: "업무 단순화 & 자동화로 엑셀을 실무에 더 적극 활용하기",
-  type: "인기",
-  status: "모집중",
-  field: "데이터분석",
-  skill: "Excel",
-  startDate: "2021-03-13",
-  thumbnail: "https://cdn.comento.kr/images/pt/tmp/prefix_44UsYDVNuM.jpg",
-};
 const communityMock: ICommunity = {
   id: 0,
   tags: ["야호", "정말"],
@@ -40,6 +29,7 @@ const communityMock: ICommunity = {
     },
   ],
 };
+import { getCampsByType } from "apis/campApi";
 
 const Home = () => {
   const [popularCamps, setPopularCamps] = useState<ICamp[]>([]);
@@ -47,17 +37,14 @@ const Home = () => {
   const [communities, setCommunities] = useState<ICommunity[]>([]);
 
   useEffect(() => {
-    // TODO: 실서버 데이터로 변경
-    setPopularCamps([campMock, campMock, campMock, campMock]);
-    setSaleCamps([campMock, campMock, campMock, campMock]);
-    setCommunities([
-      communityMock,
-      communityMock,
-      communityMock,
-      communityMock,
-    ]);
+    fetchCamps("popular");
+    fetchCamps("sale");
   }, []);
 
+  const fetchCamps = async (type: CampType) => {
+    const camps = await getCampsByType(type);
+    type === "popular" ? setPopularCamps(camps) : setSaleCamps(camps);
+  };
   return (
     <Container>
       <Navigation />
